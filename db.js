72e4666bc216c16fa7e84dedb2ceee179a2098d0
dpaghari <dspagharion@gmail.com>
2017-02-22 23:10:40 -0800

    changed db settings to heroku sigh

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql');
 
 var dbWrapper = (function(mysql) {
-  var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password: 'root',
-    database : 'homecooked'
-  });
   // var connection = mysql.createConnection({
-  //   host : 'us-cdbr-iron-east-04.cleardb.net',
-  //   user : 'b13efb3091028d',
-  //   password: '5be8e122',
-  //   database : 'heroku_ca9001ccd1a294b'
+  //   host : 'localhost',
+  //   user : 'root',
+  //   password: 'root',
+  //   database : 'homecooked'
   // });
+  var connection = mysql.createConnection({
+    host : 'us-cdbr-iron-east-04.cleardb.net',
+    user : 'b13efb3091028d',
+    password: '5be8e122',
+    database : 'heroku_ca9001ccd1a294b'
+  });
 
 
   return {
