ff892018e288c2ce01ee02c0835bb37df078abf7
dpaghari <dspagharion@gmail.com>
2017-03-01 00:45:08 -0800

    fixed forreal

--- js/Homecooked.js
+++ js/Homecooked.js
@@ -65,7 +65,6 @@ const Homecooked = (function() {
 
       let formatted_ingredients = JSON.parse(ingredients);
       let formatted_directions = JSON.parse(instructions);
-      blurb = typeof blurb === "string" ? JSON.parse(blurb) : blurb;
       let ing_string = "", dir_string = "";
 
       if(formatted_ingredients !== null){
