4d4fbd74e3cb9072c78feedf578efe6ce82b1826
Clayton Wang <claydwang@gmail.com>
2018-10-24 10:01:48 -0700

    week dates

--- src/js/client/components/MealPlan.js
+++ src/js/client/components/MealPlan.js
@@ -1,48 +1,83 @@
 import React from 'react';
 
-
 export default class MealPlan extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      currentWeek: '',
+      currentDate: '',
+      currentDay: ' '
     };
+  }
+
+  componentDidMount() {
+    var today = new Date();
+    var day = today.getDay();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    var startOfTheWeek;
+    var endOfTheWeek;
+    if (day) {
+      startOfTheWeek = mm + '/' + (dd - day) + '/' + yyyy;
+      endOfTheWeek = mm + '/' + (dd + (6 - day)) + '/' + yyyy;
+    }
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+    this.setState({
+      currentWeek: startOfTheWeek + '-' + endOfTheWeek,
+      currentDate: today,
+      currentDay: day
+    });
   }
 
   render() {
     return (
       <section class="c-mealplan">
-        <div class="c-mealplan__header">Jan 7 - 13</div>
+        <div class="c-mealplan__header">
+          {this.state.currentDate}
+          <br />
+          {this.state.currentDay}
+          <br />
+          {this.state.currentWeek}
+          <br />
+        </div>
         <div class="c-menu">
           <div class="c-menu__day">
+            <span class="c-menu__day__label">Sun</span>
+            <ul class="c-menu__day__list" />
+          </div>
+          <div class="c-menu__day">
             <span class="c-menu__day__label">Mon</span>
-            <ul class="c-menu__day__list"></ul>
+            <ul class="c-menu__day__list" />
           </div>
           <div class="c-menu__day">
             <span class="c-menu__day__label">Tue</span>
-            <ul class="c-menu__day__list"></ul>
+            <ul class="c-menu__day__list" />
           </div>
           <div class="c-menu__day">
             <span class="c-menu__day__label">Wed</span>
-            <ul class="c-menu__day__list"></ul>
+            <ul class="c-menu__day__list" />
           </div>
           <div class="c-menu__day">
-            <span class="c-menu__day__label">Thu</span>
-            <ul class="c-menu__day__list"></ul>
+            <span class="c-menu__day__label">Thur</span>
+            <ul class="c-menu__day__list" />
           </div>
           <div class="c-menu__day">
             <span class="c-menu__day__label">Fri</span>
-            <ul class="c-menu__day__list"></ul>
+            <ul class="c-menu__day__list" />
           </div>
           <div class="c-menu__day">
             <span class="c-menu__day__label">Sat</span>
-            <ul class="c-menu__day__list"></ul>
-          </div>
-          <div class="c-menu__day">
-            <span class="c-menu__day__label">Sun</span>
-            <ul class="c-menu__day__list"></ul>
+            <ul class="c-menu__day__list" />
           </div>
         </div>
+        <button>next week</button>
       </section>
     );
   }
--- src/js/client/pages/Home.js
+++ src/js/client/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Dashboard from './Dashboard';
+import MealPlanner from '../components/MealPlan.js';
 
 // Components
 import Login from '../components/Login';
@@ -8,14 +9,24 @@ import Login from '../components/Login';
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
-
     let { appState, updateAppState, logInSuccess } = this.props;
-    if(appState.currentUser)
-      return <Dashboard updateAppState={updateAppState} appState={appState} />;
+    if (appState.currentUser)
+      return (
+        <div>
+          <Dashboard updateAppState={updateAppState} appState={appState} />
+          <MealPlanner />
+        </div>
+      );
     else
-      return <Login updateAppState={updateAppState.bind(this)} logInSuccess={logInSuccess.bind(this)}/>;
+      return (
+        <Login
+          updateAppState={updateAppState.bind(this)}
+          logInSuccess={logInSuccess.bind(this)}
+        />
+      );
   }
 }
